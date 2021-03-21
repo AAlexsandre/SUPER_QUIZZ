8b3d8ed0447740395223f054eb090c37a468cc0f
Alexsandre Akofovi <52098@etu.he2b.be>
2021-03-21 18:48:00 +0100

    perso Étape 1.3 : Le formulaire de choix du quiz : ajustement du code

--- javascript/quizzJavascript.js
+++ javascript/quizzJavascript.js
@@ -1,4 +1,6 @@
-//-------------------------------------------------------//
+/**
+ * This function display in the console the id of quizz and titles
+ */
 function browseTable() {
     for (const key in quizzes) {
         console.log(key + " = " + quizzes[key].title);
@@ -8,10 +10,13 @@ function browseTable() {
 $("document").ready(function () {
     browseTable();
 });
-//-------------------------------------------------------//
-//-------------------------------------------------------//
+
+
+/**
+ * this function creates a form and dynamically adds the drop-down list 
+ */
 function createListe(){
-    $("header").after("<form action=>CHOIX DU QUIZZ</form>");
+    $("header").after("<form action=><h2>CHOIX DU QUIZZ<h2></form>");
     $("form").append("<select></select>");
     for (const key in quizzes) {
         $("select").append("<option>"+quizzes[key].title+"</option>");
