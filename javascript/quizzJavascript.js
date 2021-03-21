cd155992eee1fbd0e163e5791223de15fca7e9ff
Alexsandre Akofovi <52098@etu.he2b.be>
2021-03-21 17:30:02 +0100

    projet2 : Étape 1.3 : Le formulaire de choix du quiz : fait

--- javascript/quizzJavascript.js
+++ javascript/quizzJavascript.js
@@ -1,9 +1,24 @@
-function browseTable(){
+//-------------------------------------------------------//
+function browseTable() {
     for (const key in quizzes) {
         console.log(key + " = " + quizzes[key].title);
     }
 }
 
-$("document").ready(function(){
+$("document").ready(function () {
     browseTable();
+});
+//-------------------------------------------------------//
+//-------------------------------------------------------//
+function createListe(){
+    $("header").after("<form action=>CHOIX DU QUIZZ</form>");
+    $("form").append("<select></select>");
+    for (const key in quizzes) {
+        $("select").append("<option>"+quizzes[key].title+"</option>");
+    }
+    $("form").append("<button type=submit> Commencer </button>");
+}
+
+$("document").ready(function () {
+    createListe();
 });
