08dcfb3cd6bd08fc9d2b5ec982282d0ae79731b3
Alexsandre Akofovi <52098@etu.he2b.be>
2021-03-24 12:14:49 +0100

    projet 2 : Étape 3.1 : Parcourir le quiz : fait

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,15 +1,24 @@
+let parameters = new URL(document.location.href).searchParams.get("choiceQuizz");
 /**
  * This function recup and display the Title and description of the quizzes
  */
 function recupId() {
-    let parameters = new URL(document.location.href);
-    let name = parameters.searchParams.get("choiceQuizz");
 
     $("header").after("<section><h1>Super Quizzzz</h1></section>");
-    $("section").append("<p>"+quizzes[name].title+"</p>");
-    $("section").append("<p>"+quizzes[name].description+"</p>");    
+    $("section").append("<p>" + quizzes[parameters].title + "</p>");
+    $("section").append("<p>" + quizzes[parameters].description + "</p>");
+}
+
+/**
+ * This function display all questions of quiz
+ */
+function DisplayTheQuestions() {
+    for(let i = 0; i < quizzes[parameters].data.length; i++){
+        console.log(quizzes[parameters].data[i].question);
+    }
 }
 
 $("document").ready(function () {
     recupId();
+    DisplayTheQuestions();
 });
