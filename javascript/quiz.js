ae7757846e99ba34f517136094cfa55df56b9a90
Alexsandre Akofovi <52098@etu.he2b.be>
2021-03-28 18:28:08 +0200

    projet 2 : Étape 4.1 : Questions à réponses multiples : fait perso : rectification du nom du dossier (jeux -> jv);

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,10 +1,8 @@
 let parameters = new URL(document.location.href).searchParams.get("choiceQuizz");
-let difId = 0;
 /**
  * This function recup and display the Title and description of the quizzes
  */
 function recupId() {
-
     $("header").after("<section><h1>Super Quizzzz</h1></section>");
     $("section").append("<p>" + quizzes[parameters].title + "</p>");
     $("section").append("<p>" + quizzes[parameters].description + "</p>");
@@ -23,19 +21,31 @@ function DisplayTheQuestionsInConsole() {
  * this function creates and displays the selected quiz on the home page 
  */
 function DisplayTheQuiz() {
+    let difId = 0;
     let index = 0;
+
     $("section").append("<form action=resultats.html></form>")
 
+    for (let i = 0; i < quizzes[parameters].data.length; i++) {
+        $("form").append("<img src=../images/" + quizzes[parameters].data[i].image + ">");
+        $("form").append("<div name=" + parameters + ">" + quizzes[parameters].data[i].question + "</div");
 
-    for (let i = 1; i <= quizzes[parameters].data.length; i++) {
-        $("form").append("<img src=../images/" + parameters + "/img" + i + ".jpg></img>");
-        $("form").append("<div name=" + parameters + ">" + quizzes[parameters].data[i - 1].question + "</div");
+        if (quizzes[parameters].data[i].bonneReponses.length == 1) {
+            for (let j = 0; j < quizzes[parameters].data[i].reponses.length; j++) {
+                $("form").append("<input type=radio id=answer" + difId + "   name=answer" + i + " value=" + index + ">");
+                $("input:last").after("<label for=answer" + difId + ">" + quizzes[parameters].data[i].reponses[j] + "</label>");
+                index++;
+                difId++;
+            }
+        }
 
-        for (let j = 0; j < quizzes[parameters].data[i - 1].reponses.length; j++) {
-            $("form").append("<input type=radio id=answer" + difId + "   name=answer" + i + " value=" + index + ">");
-            $("input:last").after("<label for=answer"+difId+">"+ quizzes[parameters].data[i - 1].reponses[j]+"</label>");
-            index++;
-            difId++;
+        else {
+            for (let j = 0; j < quizzes[parameters].data[i].reponses.length; j++) {
+                $("form").append("<input type=checkbox id=answer" + difId + "   name=answer" + i + " value=" + index + ">");
+                $("input:last").after("<label for=answer" + difId + ">" + quizzes[parameters].data[i].reponses[j] + "</label>");
+                index++;
+                difId++;
+            }
         }
 
         $("form").append("<br>");
@@ -43,7 +53,6 @@ function DisplayTheQuiz() {
     }
     $("form").append("<input name=choiceQuizz type=hidden value=" + parameters + ">");
     $("form").append("<button class=check type=submit>Verification</button>");
-
 }
 
 $("document").ready(function () {
--- javascript/resultats.js
+++ javascript/resultats.js
@@ -5,28 +5,54 @@ function createAndDisplayTheAnswers() {
     $("#placeForLoadBar").remove();
     let quiz = new URL(document.location.href).searchParams.get("choiceQuizz");
     let tabAnswer = [];
-    let i = 1;
+    let i = 0;
 
-    tabAnswer[i] = new URL(document.location.href).searchParams.get("answer" + (i));
+    tabAnswer[i] = new URL(document.location.href).searchParams.getAll("answer" + (i));
 
-    while (tabAnswer[i] != null) {
+    while (tabAnswer.length < quizzes[quiz].data.length) {
         i++;
-        tabAnswer[i] = new URL(document.location.href).searchParams.get("answer" + (i));
+        tabAnswer[i] = new URL(document.location.href).searchParams.getAll("answer" + (i));
     }
-    tabAnswer.shift();
-    tabAnswer.pop();
+
+    console.log(tabAnswer);
 
     $("header").after("<section id=displayAnswers></section>");
     for (let index = 0; index < quizzes[quiz].data.length; index++) {
         $("#displayAnswers").append("<h2>" + quizzes[quiz].data[index].question + "</h2>");
-        $("#displayAnswers").append("<h3> Votre réponse : " + quizzes[quiz].data[index].reponses[tabAnswer[index]] + "</h3>");
 
-        if (tabAnswer[index] == quizzes[quiz].data[index].bonneReponses[0]) {
-            $("#displayAnswers").append("<p class=good> C'est la bonne réponse</p>");
+        if (quizzes[quiz].data[index].bonneReponses.length > 1) {
+
+            for (let j = 0; j < tabAnswer[index].length; j++) {
+                i = 0;
+                for (let k = 0; k < quizzes[quiz].data[index].bonneReponses.length; k++) {
+                    if (tabAnswer[index][j] == quizzes[quiz].data[index].bonneReponses[k]) {
+                        $("#displayAnswers").append("<h3> Votre réponse : " + quizzes[quiz].data[index].reponses[tabAnswer[index][j]] + "</h3>");
+                        $("#displayAnswers").append("<p class=good> C'est la bonne réponse</p>");
+                        i++;
+                    }
+                }
+                if (i == 0) {
+                    $("#displayAnswers").append("<h3> Votre réponse : " + quizzes[quiz].data[index].reponses[tabAnswer[index][j]] + "</h3>");
+                    if (tabAnswer[index][j+1] == quizzes[quiz].data[index].bonneReponses[j]) {
+                        $("#displayAnswers").append("<p class=bad>faux. La bonne réponse est : " + quizzes[quiz].data[index].reponses[quizzes[quiz].data[index].bonneReponses[j+(quizzes[quiz].data[index].bonneReponses.length-1)]] + "</p>");
+                    }
+                    if (tabAnswer[index][j+1] == quizzes[quiz].data[index].bonneReponses[j+1]) {
+                        $("#displayAnswers").append("<p class=bad>faux. La bonne réponse est : " + quizzes[quiz].data[index].reponses[quizzes[quiz].data[index].bonneReponses[j]] + "</p>");
+                    }
+                }
+            }
 
         } else {
-            $("#displayAnswers").append("<p class=bad>faux. La bonne réponse est : " + quizzes[quiz].data[index].reponses[quizzes[quiz].data[index].bonneReponses[0]] + "</p>");
+            $("#displayAnswers").append("<h3> Votre réponse : " + quizzes[quiz].data[index].reponses[tabAnswer[index]] + "</h3>");
+
+            if (tabAnswer[index] == quizzes[quiz].data[index].bonneReponses[0]) {
+                $("#displayAnswers").append("<p class=good> C'est la bonne réponse</p>");
+
+            } else {
+                $("#displayAnswers").append("<p class=bad>faux. La bonne réponse est : " + quizzes[quiz].data[index].reponses[quizzes[quiz].data[index].bonneReponses[0]] + "</p>");
+            }
         }
+
     }
 }
 
@@ -39,14 +65,14 @@ function loadingQuiz() {
     $("#placeForLoadBar").append("<div id=loadBar></div>");
     $("#loadBar").append("<div id=loading ></div>");
 
-    for (let i = 0; i < 2; i++) {
-        $("#loading").animate({ left: "38em" }, 1000);
-        $("#loading").animate({ left: "0em" }, 1000);
+    for (let i = 0; i < 4; i++) {
+        $("#loading").animate({ left: "38em" }, 500);
+        $("#loading").animate({ left: "0em" }, 500);
     }
 
 }
 
 $("document").ready(function () {
     loadingQuiz();
-    setTimeout(createAndDisplayTheAnswers,4500);
+    setTimeout(createAndDisplayTheAnswers, 4500);
 });
